f9f11d9fd098e82ee837d10aaba3b12a14f7c70e
dimitori <dimitorysama@gmail.com>
2020-08-23 00:18:09 +0300

    pytest

--- README.md
+++ README.md
@@ -3,11 +3,12 @@
 ![Easy peasy](https://user-images.githubusercontent.com/1592663/79918184-93bca100-8434-11ea-9902-0ff726a864a3.gif)
 
 
-This repo inspired by https://github.com/f213/django
+This repo inspired by https://github.com/f213/django and https://github.com/pydanny/cookiecutter-django
 
 ## What is in the box
 
 ## Optional next steps
+
 
 ## Installation
 
--- {{cookiecutter.project_slug}}/src/app/settings.py
+++ {{cookiecutter.project_slug}}/src/app/settings.py
@@ -10,6 +10,8 @@ SITE_ROOT = root()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Disable built-in ./manage.py test command in favor of pytest
+TEST_RUNNER = 'app.test.disable_test_command_runner.DisableTestCommandRunner'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
